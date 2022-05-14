8dc491797c1fcbede86512c9a6c69c2bc913c0bb
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2022-05-14 20:03:25 +0200

    switch from libvlc to opencv

--- main.cpp
+++ main.cpp
@@ -1,35 +1,57 @@
 #include <iostream>
 #include <string>
+#include <fmt/core.h>
 
-#include <vlcpp/vlc.hpp>
-#include <vlcpp/MediaPlayer.hpp>
+#include <opencv2/opencv.hpp>
 
-const std::string cam_ip = "192.168.178.30";    /**< Camera's IP address */
-const std::string cam_port = "554";             /**< Camera's RTSP server port */
+
+const int min_argc_num = 5;  /**< minimum number of arguments that must be passed to the program */
 
 /**
- * @brief print debug string if DEBUG flag is enabled
+ * @brief print debug string if DEBUG macro is defined
  * 
- * @param s string to be printed
+ * @param str string to be printed
  */
-void debug_str(std::string s) {
+void debug_print(const char *str) {
     #ifdef DEBUG
-    std::cout << s << std::endl;
+    std::cout << str << std::endl;
     #endif
 }
 
-int main() {
-    debug_str("Entering main");
+int main(int argc, const char* argv[]) {
+    if (argc < min_argc_num) {
+        std::cerr << "Wrong arguments" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " CAM_IP CAM_PORT USERNAME PASSWORD" << std::endl;
+        return 1;
+    }
 
-    std::string url = "rtsp://" + cam_ip + ":" + cam_port;
-    const char* const options[] = {
-        "--sout-rtsp-user", "admin",
-        "--sout-rtsp-pwd", "password"};
-    auto instance = VLC::Instance(4, options);
-    auto md = VLC::Media(instance, url, VLC::Media::FromLocation);
-    auto player = VLC::MediaPlayer(md);
-    player.play();
+    const char *cam_ip = argv[1];
+    const char *cam_port = argv[2];
+    const char *username = argv[3];
+    const char *password = argv[4];
 
-    debug_str("Exiting from main");
+    debug_print(cam_ip);
+    debug_print(cam_port);
+    debug_print(username);
+    debug_print(password);
+
+    const std::string mrl = fmt::format("rtsp://{}:{}@{}:{}/h.265", username, password, cam_ip, cam_port);
+    debug_print(mrl.c_str());
+
+    cv::VideoCapture camera(mrl);
+    if (!camera.open(mrl)) {
+        std::cerr << "Error while opening network video stream" << std::endl;
+        return 2;
+    }
+    debug_print("Video stream opened");
+    
+    // TODO: open RTSP connection
+    // TODO: initialize FSM
+    // TODO: get frames
+    // TODO: pass frames through YOLO
+    // TODO: match bounding boxes
+    // TODO: update FSM
+    // TODO: send MQTT message (with callback?)
+    
     return 0;
 }
