6e06bda9ad59f46c49003053aab3356049eb801d
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2022-05-18 21:42:19 +0200

    divide detectio code into multiple helper functions

--- object_detection/object_detection.cpp
+++ object_detection/object_detection.cpp
@@ -27,6 +27,52 @@ static const float YOLO_HEIGHT_F = 640.;
     return res;
 }
 
+[[nodiscard]] std::tuple<std::vector<int>, std::vector<float>, std::vector<cv::Rect>> ObjectDetector::unwrap_yolo5_result(const cv::Mat &img, const std::vector<cv::Mat> &predictions, float score_threshold) const {
+    const float x_factor = img.cols / YOLO_WIDTH_F;
+    const float y_factor = img.rows / YOLO_HEIGHT_F;
+    float *data = (float*) predictions[0].data;
+    const int dimentions = 85;
+    const int rows = 25200;
+    
+    std::vector<int> ids;
+    std::vector<float> confidences;
+    std::vector<cv::Rect> boxes;
+    for (int i=0; i<rows; i++) {
+        float confidence = data[4];
+        if (confidence >= .4) {
+            float *classes_scores = data+5;
+            cv::Mat scores(1, ids.size(), CV_32FC1, classes_scores);
+            cv::Point class_id;
+            double max_class_score;
+            minMaxLoc(scores, 0, &max_class_score, 0, &class_id);
+            if (max_class_score > score_threshold) {
+                confidences.push_back(confidence);
+                ids.push_back(class_id.x);
+
+                float x = data[0];
+                float y = data[1];
+                float w = data[2];
+                float h = data[3];
+                int left = int((x - 0.5 * w) * x_factor);
+                int top = int((y - 0.5 * h) * y_factor);
+                int width = int(w * x_factor);
+                int height = int(h * y_factor);
+                boxes.push_back(cv::Rect(left, top, width, height));
+            }
+        }
+
+        data += dimentions;
+    }
+
+    return {ids, confidences, boxes};
+}
+
+[[nodiscard]] std::vector<int> ObjectDetector::filter_bounding_boxes(const std::vector<cv::Rect> &boxes, const std::vector<float> &confidences, float score_threshold, float nms_threshold) const {
+    std::vector<int> result;
+    cv::dnn::NMSBoxes(boxes, confidences, score_threshold, nms_threshold, result);
+    return result;
+}
+
 [[nodiscard]] std::vector<Detection> ObjectDetector::detect(const cv::Mat &frame) const {
     auto square_frame = format_for_yolo5(frame);
 
@@ -34,8 +80,18 @@ static const float YOLO_HEIGHT_F = 640.;
     auto net_copy = this->net;
     net_copy.forward(predictions, net.getUnconnectedOutLayersNames());
 
-    // unwrap_yolo5_result
-    
+    auto [class_ids, confidences, bounding_boxes] = unwrap_yolo5_result(frame, predictions, .25);
 
-    // filter_bounding_boxes
+    auto filtered_boxes_idx = filter_bounding_boxes(bounding_boxes, confidences, .25, .45);
+
+    std::vector<Detection> output;
+    for (int i=0; i<filtered_boxes_idx.size(); i++) {
+        int idx = filtered_boxes_idx[i];
+        output.push_back({
+            .class_id   = class_ids[idx],
+            .confidence = confidences[idx],
+            .box        = bounding_boxes[idx]
+        });
+    }
+    return output;
 }
--- object_detection/object_detection.h
+++ object_detection/object_detection.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <tuple>
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/dnn.hpp>
@@ -16,9 +17,34 @@ namespace objdet {
         private:
             cv::dnn::Net net;  /**< neural network */
 
+            /**
+             * @brief Format source frame for YOLOv5 network
+             * 
+             * @param source image
+             * @return cv::Mat square image ready to be processed by YOLO
+             */
             cv::Mat format_for_yolo5(const cv::Mat &source) const;
-            // filter_bounding_boxes
-            // unwrap_yolo5_result
+
+            /**
+             * @brief Unwrap YOLOv5 result into three vectors
+             * 
+             * @param img input frame
+             * @param predictions input predictions vector
+             * @param score_threshold intput confidence threshold
+             * @return std::tuple<std::vector<int>, std::vector<float>, std::vector<cv::Rect>> 
+             */
+            std::tuple<std::vector<int>, std::vector<float>, std::vector<cv::Rect>> unwrap_yolo5_result(const cv::Mat &img, const std::vector<cv::Mat> &predictions, float score_threshold) const;
+
+            /**
+             * @brief Filter bounding boxes that come from the YOLO net and keep non overlapping ones
+             * 
+             * @param boxes input boxes
+             * @param confidences input confidence vector
+             * @param score_threshold input confidence threshold
+             * @param nms_threshold input nms threshold
+             * @return std::vector<int> output indexes that are not overlapping
+             */
+            std::vector<int> filter_bounding_boxes(const std::vector<cv::Rect> &boxes, const std::vector<float> &confidences, float score_threshold, float nms_threshold) const ;
         public:
             ObjectDetector(ObjectDetector &od) = default;
 
